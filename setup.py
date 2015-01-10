0b3145fc1f7abf439c7d66fa83708095496745ae
Patrik Hermansson <patrik@paheco.nu>
2015-01-10 08:41:11 +0100

    Pycharm test

--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
 __author__ = 'user'
 
 from distutils.core import setup
-setup(name='autospot',
-      version='1.0',
-      py_modules=['AutoSpot'],
-      author='Patrik Hermansson',
-      author_email='patrik@paheco.nu',
-      url='https://github.com/bphermansson/AutoSpot/',
-      )
+from pkg_resources import Requirement, resource_filename
+from setuptools import setup, find_packages
 
-install_requires=[
-    'pyspotify'
-]
+setup(name='autospot',
+    version='1.0',
+    packages = find_packages(),
+    scripts = ['AutoSpot.py'],
+    package_data = {
+
+    # If any package contains *.txt or *.rst files, include them:
+    '': ['*.txt'],
+    },
+    author='Patrik Hermansson',
+    author_email='patrik@paheco.nu',
+    url='https://github.com/bphermansson/AutoSpot/',
+    install_requires=['pyspotify >= 2.0.0b3']
+)
+
+
+
+filename = resource_filename(Requirement.parse("autospot"),"settings_editthis")
